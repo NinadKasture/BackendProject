3a97322d5a535d219c1d588f87bc21cc9a9af629
Ninad <ninadkasture98@gmail.com>
2024-09-09 10:29:24 +0530

    multer and clodinary used

--- src/utils/apierrors.js
+++ src/utils/apierrors.js
@@ -3,16 +3,16 @@ class apiError extends Error {
         statusCode,
         message = "Something went wrong",
         errors=[],
-        statck=""
+        stack=""
     ){
         super(message)
         this.statusCode=statusCode
         this.data = null
         this.message = message
         this.success = false;
-        this.errors = this.errors
+        this.errors = errors
 
-        if(statck){
+        if(stack){
             this.stack = stack
         }else{
             Error.captureStackTrace(this,this.constructor)
